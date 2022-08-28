7487aa613b1891070ff613e185bc1788fa735ef1
Efim Shchegolev <efimblashck@yandex.ru>
2022-08-28 16:36:09 +0300

    feat: добавить получение информации о пользователе

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,18 +1,35 @@
+import { useEffect, useState } from "react";
+import api from "../api";
+
 function Main(props) {
   const { onEditProfile, onAddPlace, onEditAvatar } = props;
+  const [userName, setUserName] = useState("");
+  const [userDescription, setUserDescription] = useState("");
+  const [userAvatar, setUserAvatar] = useState("");
+  useEffect(() => {
+    api.getUserInfoFromServer().then((data) => {
+      setUserName(`${data.name}`);
+      setUserDescription(`${data.about}`);
+      setUserAvatar(`${data.avatar}`);
+    });
+  }, []);
   return (
     <>
       <section className="profile">
         <div className="profile__avatar-container" onClick={onEditAvatar}>
-          <img src="" className="profile__avatar" alt="Аватарка" />
+          <img
+            src={`${userAvatar}`}
+            className="profile__avatar"
+            alt="Аватарка"
+          />
         </div>
         <div className="profile__info">
-          <h1 className="profile__name">Жак-Ив Кусто</h1>
+          <h1 className="profile__name">{userName}</h1>
           <button
             className="profile__edit-button"
             onClick={onEditProfile}
           ></button>
-          <h2 className="profile__about">Исследователь океана</h2>
+          <h2 className="profile__about">{userDescription}</h2>
         </div>
         <button className="profile__add-button" onClick={onAddPlace}></button>
       </section>
@@ -22,3 +39,9 @@ function Main(props) {
 }
 
 export default Main;
+
+// about: "Хороший мальчик"
+// avatar: "https://cdn.fishki.net/upload/post/201512/11/1771317/tn/cb43e36ba9eea7b2dcd3410d99262de8.jpg"
+// cohort: "cohort-47"
+// name: "ЖакЖак"
+// _id: "b8ccfc62ba05e7b825498188"
